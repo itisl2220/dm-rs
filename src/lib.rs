423c3ca71053a76f1c1d712b8351d2ab0b4993c6
ITisl <1831207432@qq.com>
2023-06-28 04:11:01 +0800

    feat: 可以调用了

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
-use std::{iter::once, ops::DerefMut, os::windows::ffi::OsStrExt, ptr};
+use std::{iter::once, os::windows::ffi::OsStrExt};
+use variant_rs::dispatch::IDispatchError;
+use variant_rs::dispatch::IDispatchExt;
+
 use windows::{
-    core::{ComInterface, IUnknown, BSTR, GUID, PCWSTR},
+    core::{ComInterface, IUnknown, PCWSTR},
     Win32::System::Com::{
         CLSIDFromProgID, CoCreateInstance, CoInitializeEx, IDispatch, CLSCTX_ALL,
-        COINIT_MULTITHREADED, DISPPARAMS, VARIANT,
+        COINIT_MULTITHREADED,
     },
 };
 fn encode_wide(string: impl AsRef<std::ffi::OsStr>) -> Vec<u16> {
@@ -13,71 +16,46 @@ fn encode_wide(string: impl AsRef<std::ffi::OsStr>) -> Vec<u16> {
 pub struct DmSoft {
     pub obj: IDispatch,
 }
-struct MyDispatchDriver {
-    obj: IDispatch,
+
+// 大漠插件错误处理
+pub enum DmError {
+    IDispatchError(IDispatchError),
+    DmError(i32),
+    WindowsError(windows::core::Error),
 }
-impl MyDispatchDriver {
-    pub fn new(obj: &IDispatch) -> Self {
-        Self { obj: obj.clone() }
-    }
 
-    pub fn get_idof_name(&self, lpsz: &str, disp_id: &mut i32) -> () {
-        let lpsz = PCWSTR::from_raw(encode_wide(lpsz).as_ptr());
-        return unsafe {
-            let _ = self.obj.GetIDsOfNames(ptr::null(), &lpsz, 1, 0, disp_id);
-        };
-    }
-
-    pub fn invoke0(&self, disp_id: i32, pvar_ret: Option<*mut VARIANT>) {
-        let dis = DISPPARAMS {
-            rgvarg: ptr::null_mut(),
-            rgdispidNamedArgs: ptr::null_mut(),
-            cArgs: 0,
-            cNamedArgs: 0,
-        };
-        return unsafe {
-            let _ = self.obj.Invoke(
-                disp_id,
-                &GUID::zeroed(),
-                0,
-                windows::Win32::System::Com::DISPATCH_METHOD,
-                &dis as *const DISPPARAMS,
-                pvar_ret,
-                None,
-                None,
-            );
-        };
-    }
-
-    pub fn invoke_n(
-        &self,
-        disp_id: i32,
-        pvar_params: *mut VARIANT,
-        n_params: u32,
-        pvar_ret: Option<*mut VARIANT>,
-    ) {
-        let dis = DISPPARAMS {
-            rgvarg: pvar_params,
-            rgdispidNamedArgs: ptr::null_mut(),
-            cArgs: n_params,
-            cNamedArgs: 0,
-        };
-        return unsafe {
-            let _ = self.obj.Invoke(
-                disp_id,
-                &GUID::zeroed(),
-                0,
-                windows::Win32::System::Com::DISPATCH_METHOD,
-                &dis,
-                pvar_ret,
-                None,
-                None,
-            );
-        };
+impl std::fmt::Debug for DmError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DmError::IDispatchError(e) => write!(f, "IDispatchError: {:?}", e),
+            DmError::DmError(e) => write!(f, "DmError: {:?}", e),
+            DmError::WindowsError(e) => write!(f, "WindowsError: {:?}", e),
+        }
     }
 }
+
+impl From<i32> for DmError {
+    fn from(e: i32) -> Self {
+        DmError::DmError(e)
+    }
+}
+
+impl From<IDispatchError> for DmError {
+    fn from(e: IDispatchError) -> Self {
+        DmError::IDispatchError(e)
+    }
+}
+
+impl From<windows::core::Error> for DmError {
+    fn from(e: windows::core::Error) -> Self {
+        DmError::WindowsError(e)
+    }
+}
+
+pub type DmResult<T> = std::result::Result<T, DmError>;
+
 impl DmSoft {
-    pub fn new() -> Result<Self, anyhow::Error> {
+    pub fn new() -> DmResult<Self> {
         unsafe {
             CoInitializeEx(None, COINIT_MULTITHREADED)?;
             let clsid = CLSIDFromProgID(PCWSTR::from_raw(encode_wide("dm.dmsoft").as_ptr()))?;
@@ -87,60 +65,24 @@ impl DmSoft {
         }
     }
 
-    pub fn ver(&self) -> Result<String, anyhow::Error> {
-        unsafe {
-            let mut disp_id: i32 = 0;
-            let mut pvar_ret = VARIANT::default();
-            let driver = MyDispatchDriver::new(&self.obj);
-            driver.get_idof_name("Ver", &mut disp_id);
-            driver.invoke0(disp_id, Some(&mut pvar_ret));
-            let result = &pvar_ret.Anonymous.Anonymous.Anonymous.bstrVal;
-            Ok(result.to_string())
-        }
+    pub fn ver(&self) -> DmResult<String> {
+        let res = self.obj.call("Ver", vec![])?;
+        Ok(res.expect_string().to_string())
     }
 
-    pub fn enable_pic_cache(&self) -> Result<String, anyhow::Error> {
-        unsafe {
-            let mut disp_id: i32 = 0;
-            let mut pvar_ret = VARIANT::default();
-
-            let mut pn: [VARIANT; 1] = [VARIANT::default()];
-            pn[0].Anonymous.Anonymous.deref_mut().Anonymous.llVal = 1;
-
-            let driver = MyDispatchDriver::new(&self.obj);
-            driver.get_idof_name("EnablePicCache", &mut disp_id);
-            driver.invoke_n(disp_id, pn.as_mut_ptr(), 1, Some(&mut pvar_ret));
-            let result = &pvar_ret.Anonymous.Anonymous.Anonymous.bstrVal;
-            Ok(result.to_string())
-        }
+    pub fn enable_pic_cache(&self) -> DmResult<i32> {
+        let res = self.obj.call("EnablePicCache", vec![1.into()])?;
+        Ok(res.expect_i32())
     }
 
-    pub fn reg(&self, code: &str, ver: &str) -> Result<i32, anyhow::Error> {
-        unsafe {
-            let mut pn: [VARIANT; 2] = [VARIANT::default(), VARIANT::default()];
-            *pn[0].Anonymous.Anonymous.deref_mut().Anonymous.bstrVal = BSTR::from(code);
-            *pn[0].Anonymous.Anonymous.deref_mut().Anonymous.bstrVal = BSTR::from(ver);
-            let mut disp_id: i32 = -1;
-            let mut pvar_ret = VARIANT::default();
-            let driver = MyDispatchDriver::new(&self.obj);
-            driver.get_idof_name("Reg", &mut disp_id);
-            driver.invoke_n(disp_id, pn.as_mut_ptr(), 2, Some(&mut pvar_ret));
-            Ok(pvar_ret.Anonymous.Anonymous.Anonymous.lVal)
-        }
+    pub fn reg(&self, code: &str, ver: &str) -> DmResult<i32> {
+        let res = self.obj.call("Reg", vec![code.into(), ver.into()])?;
+        let res: i32 = res.expect_i32();
+        Ok(res)
     }
 
-    pub fn set_row_gap_no_dict(&self, row_gap: i64) -> Result<i32, anyhow::Error> {
-        unsafe {
-            let mut pn = [VARIANT::default()];
-            (*pn[0].Anonymous.Anonymous).Anonymous.llVal = row_gap;
-            let mut disp_id: i32 = 0;
-            let mut pvar_ret = VARIANT::default();
-            let driver = MyDispatchDriver::new(&self.obj);
-            driver.get_idof_name("SetRowGapNoDict", &mut disp_id);
-            driver.invoke_n(disp_id, pn.as_mut_ptr(), 1, Some(&mut pvar_ret));
-            let pvar_ret = pvar_ret.Anonymous.Anonymous.Anonymous.lVal;
-
-            return Ok(pvar_ret);
-        }
+    pub fn set_row_gap_no_dict(&self, row_gap: i64) -> DmResult<i32> {
+        let res = self.obj.call("SetRowGapNoDict", vec![row_gap.into()])?;
+        Ok(res.expect_i32())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use dm_rs::DmResult;
 use dm_rs::DmSoft;
 
-fn main() -> Result<(), anyhow::Error> {
+fn main() -> DmResult<()> {
     let obj = DmSoft::new();
     match obj {
         Ok(obj) => {
@@ -8,6 +9,8 @@ fn main() -> Result<(), anyhow::Error> {
             println!("ver:{:?}", ver);
             let v = obj.reg("mh84909b3bf80d45c618136887775ccc90d27d7", "mmqnvy80ddz0ec7")?;
             println!("v:{:?}", v);
+            let res = obj.enable_pic_cache()?;
+            println!("res:{:?}", res);
         }
         Err(e) => {
             println!("{:?}", e);
